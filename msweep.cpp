2840b605c90ef0e087f812058909f636b92836d3
amongsusman <149024306+amongsusman@users.noreply.github.com>
2025-02-21 09:20:25 -0500

    Update msweep.cpp

--- msweep.cpp
+++ msweep.cpp
@@ -5,10 +5,21 @@ Date: 2/19/25
 Extra:
 */
 #include <iostream>
+#include <algorithm>
+#include <fstream>
+#include <random>
+#include <vector>
+#include <chrono>
+#include <thread>
+#include <string>
+#include <cstdlib>
 using namespace std;
-#include <ctime>
+
 void playGame(int length, int width, int mines);
 void howToPlay();
+bool check(int i, int j, int length, int width);
+string emoji(int num);
+void cs();
 
 int main()
 {
@@ -31,7 +42,7 @@ int main()
     cout <<"How many mines should there be? " << endl;
     cin >> mines;
     // pick size
-    cout << "Play";
+    cout << "Play" << endl;
     playGame(length, width, mines);
 
     return 0;
@@ -39,18 +50,25 @@ int main()
 
 void playGame(int length, int width, int mines) {
     // Start random here so it is different every game run
-    cout << "Start!";
+    cout << "Start!" << endl;
     srand(time(nullptr));
     // generate grid
     int grid[length][width]; // expression must have a constant value
+    string displayGrid[length][width];
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < width; j++) {
+            displayGrid[i][j] = "🟩 ";
+            grid[i][j] = 0;
+        }
+    }
     // place mines
     // while loop
+    
     int minePos[mines*2]; // expression must have a constant value
     for (int i = 0; i < (mines * 2); i+=2){
         int rand1, rand2;
         bool good = true;
         do {
-            good = true;
             rand1 = rand() % length;
             rand2 = rand() % width;
             for (int j = 0; j < (mines * 2); j += 2) {
@@ -69,37 +87,138 @@ void playGame(int length, int width, int mines) {
     for (int i = 0; i < (mines * 2); i += 2) {
         grid[minePos[i]][minePos[i + 1]] = 9;
     }
-
-    /*for (int i = 0; i < length; i++) {
-        for (int j = 0; j )
-    }*/
+    for (int i = 0; i < length; i++) {
+        for (int j = 0; j < width; j++) {
+            int count = 0;
+            if (grid[i][j] != 9) {
+                if (check(i-1, j-1, length, width) && grid[i - 1][j - 1] == 9){
+                    count++;
+                }
+                if (check(i, j-1, length, width) && grid[i][j - 1] == 9){
+                    count++;
+                }
+                if (check(i+1, j-1, length, width) && grid[i + 1][j - 1] == 9){
+                    count++;
+                }
+                if (check(i-1, j, length, width) && grid[i - 1][j] == 9){
+                    count++;
+                }
+                if (check(i+1, j, length, width) && grid[i +1][j] == 9){
+                    count++;
+                }
+                if (check(i- 1, j + 1, length, width) && grid[i - 1][j + 1] == 9){
+                    count++;
+                }
+                if (check(i, j+1, length, width) && grid[i][j + 1] == 9){
+                    count++;
+                }
+                if (check(i +1, j + 1, length, width) && grid[i + 1][j + 1] == 9){
+                    count++;
+                }
+                grid[i][j] = count;
+            }
+        }
+    }
+    bool run = true;
+    while (run) {
+        cs();
+        for (int i = 0; i < length; i++) {
+            for (int j = 0; j < width; j++) {
+                cout << displayGrid[i][j];
+            }
+        cout << "\n";
+        }
+        char cf;
+        int ro, co;
+        cout << "Would you like to check or flag a tile? c/f\n>> ";
+        cin >> cf;
+        if (cf == 'c') {
+            cout << "What row would you like to check? \n>> ";
+            cin >> ro;
+            cout << "What column would you like to check? \n>> ";
+            cin >> co;
+            displayGrid[ro -1][co-1] = emoji(grid[ro - 1][co - 1]);
+            if(grid[ro - 1][co - 1] == 9){
+                for (int i = 0; i < length; i++) {
+                    for (int j = 0; j < width; j++) {
+                        cout << displayGrid[i][j];
+                    }
+                cout << "\n";
+                }
+                cout << "GAME OVER" << endl;
+                run = false;
+            }
+        }
+        else if (cf == 'f') {
+            cout << "What row would you like to flag? \n>> ";
+            cin >> ro;
+            cout << "What column would you like to flag? \n>> ";
+            cin >> co;
+            if (displayGrid[ro - 1][co - 1] == "🟩 ") {
+                displayGrid[ro-1][co-1]= "🚩 ";
+            }else if (displayGrid[ro - 1][co - 1] == "🚩 ") {
+                displayGrid[ro-1][co-1]= "🟩 ";
+            }
+        }
+    }
 }
-
+bool check(int i, int j, int length, int width) {
+    return (0 <= i && i < length && 0 <= j && j < width);
+}
+string emoji(int num) {
+    switch(num){
+        case 0:
+            return "🟨 ";
+        case 1:
+            return "1️⃣ ";
+        case 2:
+            return "2️⃣ ";
+        case 3:
+            return "3️⃣ ";
+        case 4:
+            return "4️⃣ ";
+        case 5:
+            return "5️⃣ ";
+        case 6:
+            return "6️⃣ ";
+        case 7:
+            return "7️⃣ ";
+        case 8:
+            return "8️⃣ ";
+        case 9:
+            return "💣 ";
+        default:
+            return "ERROR";
+    }
+}
 void howToPlay() {
+    cout << "------------------------------------------------------------------------------------------------------" << endl;
     cout << "This is minesweeper - a game about finding and avoiding mines" << endl;
     cout << "There will be mines on the board and you have to try and find them" << endl;
     cout << "If you find a spot without a mine it will have a number with the number of mines around it" << endl;
     cout << "Here is an example: " << endl;
-    cout << "🟩🟩🟩" << endl;
-    cout << "🟩 2🟩" << endl;
-    cout << "🟩🟩🟩" << endl;
+    cout << "🟩 🟩 🟩" << endl;
+    cout << "🟩 2️⃣ 🟩" << endl;
+    cout << "🟩 🟩 🟩" << endl;
     cout << "That means that in the 3x3 area around the 2 there are 2 mines" << endl;
     cout << "If there is a mine, place a flag in that spot" << endl;
     cout << "Here is an example: " << endl;
-    cout << "🟩🟩🟥" << endl;
-    cout << "🟩 2🟩" << endl;
-    cout << "🟩🟥🟩" << endl;
-    cout << "In this scenario, the user might place a flag in the wrong spot and the mine explodes!" << endl;
+    cout << "🟩 🟩 🟥" << endl;
+    cout << "🟩 2️⃣ 🟩" << endl;
+    cout << "🟩 🟥 🟩" << endl;
+    cout << "In this scenario, the user might check the wrong spot and the mine explodes!" << endl;
     cout << "Here is an example: " << endl;
-    cout << "🟩🟩🟥" << endl;
-    cout << "🟩 2🟩" << endl;
-    cout << "🟥💥🟩" << endl;
+    cout << "🟩 🟩 🟥" << endl;
+    cout << "🟩 2️⃣ 🟩" << endl;
+    cout << "🟥 💣 🟩" << endl;
     cout << "After you correctly guess where the mines are you can check the other spots" << endl;
     cout << "Here is an example: " << endl;
-    cout << "🟨1🟥" << endl;
-    cout << "1 2 2" << endl;
-    cout << "1🟥1" << endl;
-    cout << "The yellow tile has no mines within a 3x3 area of it, so it displays as empty. If a group of empty tiles are clicked they all appear at the same time" << endl;
+    cout << "🟨 1️⃣ 🟥" << endl;
+    cout << "1️⃣ 2️⃣ 2️⃣" << endl;
+    cout << "1️⃣ 🟥 1️⃣" << endl;
+    cout << "The yellow tile has no mines within a 3x3 area of it, so it displays as empty." << endl;
+    cout << "This is the end of the tutorial, pick your board size and get playing!" << endl;
+    cout << "------------------------------------------------------------------------------------------------------" << endl;
 }
 
 /* EMOJI ART:
@@ -109,5 +228,17 @@ void howToPlay() {
 🟥 - unchecked tile - mine 11
 💣 - mine not exploded 12 
 💥 - mine exploded 13
-1️⃣
+1️⃣2️⃣3️⃣4️⃣5️⃣
 */
+
+void cs() {
+    {
+    #ifdef _WIN32
+            // Windows
+            system("cls");
+    #else
+            // Unix-like systems (Linux, macOS)
+            system("clear");
+    #endif
+        }
+}
